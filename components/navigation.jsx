c93a0357eb3ea3b96ace72a97a14f3011d96460c
CJ <hjiedueh@gmail.com>
2023-11-07 03:27:27 -0500

    working on header

---
+++ components/navigation.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import { Container } from './container'
+import Image from 'next/image'
+
+
+export const Navigation = () => {
+
+    return <div className='header'>
+        <nav>
+            <Container>Hi</Container>
+        </nav>
+    </div>
+}
